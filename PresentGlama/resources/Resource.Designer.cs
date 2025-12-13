889c763a1db48c38ea4e1b5f2cf59314ae08c94e
Nickleus Jimenez <Nickleus-J@live.com.ph>
2025-12-13 23:15:55 +0800

    Dark mode correction

--- PresentGlama/resources/Resource.Designer.cs
+++ PresentGlama/resources/Resource.Designer.cs
@@ -19,7 +19,7 @@ namespace PresentGlama.resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resource {
@@ -72,9 +72,9 @@ namespace PresentGlama.resources {
         /// <summary>
         ///   Looks up a localized string similar to Use dark mode.
         /// </summary>
-        public static string DarkMdoeToggleLabel {
+        public static string DarkModeToggleLabel {
             get {
-                return ResourceManager.GetString("DarkMdoeToggleLabel", resourceCulture);
+                return ResourceManager.GetString("DarkModeToggleLabel", resourceCulture);
             }
         }
         
