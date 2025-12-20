dd0bcf28dbfb8b30413c0cf193f07d0bbcb5cfc1
Nickleus Jimenez <Nickleus-J@live.com.ph>
2025-12-21 03:42:34 +0800

    Modification related to darkmode checkbox

--- PresentGlama/wwwroot/js/site.js
+++ PresentGlama/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const useDarkCheckbox = document.getElementById('useDark');
+
+useDarkCheckbox.addEventListener('change', function () {
+    let bodyElem = document.querySelector("body");
+    if (this.checked) {
+        bodyElem.classList.add("dark");
+    } else {
+        bodyElem.classList.remove("dark");
+    }
+});
